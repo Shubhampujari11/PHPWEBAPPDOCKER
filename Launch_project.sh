349b3efaf1d11549bdab6761cd379de9dc6ae9af
Shubhampujari11 <122546945+Shubhampujari11@users.noreply.github.com>
2023-07-23 16:14:32 +0530

    Update Launch_project.sh

--- Launch_project.sh
+++ Launch_project.sh
@@ -6,6 +6,27 @@ yum install docker -y
 systemctl enable docker
 systemctl start docker
 
+echo -e "############## Installing Docker-compose.... ############"
+
+## THIS IS THE OLD WAY
+## Nowadays, simply follow the Compose installation instructions in the official documentation:
+## https://docs.docker.com/compose/install/
+
+
+# get latest docker compose released tag
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+
+# Install docker-compose
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
+sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Output compose version
+docker-compose -v
+
+#sudo su - root
+
+
 echo -e "######## DOCKER STARTUP COMPLETED !!!! ###### "
 echo -e "\n\n########### Starting docker compose and build with in #######\n\n"
 sleep 5
