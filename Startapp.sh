e656f7aa0b68a8da2d7952d8177eec48e1a47f1d
Shubhampujari11 <122546945+Shubhampujari11@users.noreply.github.com>
2023-07-24 15:13:18 +0530

    Create Startapp.sh

---
+++ Startapp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "\n\n########### Starting docker compose and build with in #######\n\n"
+sleep 5
+echo -e "Running docker compose..."
+sudo docker-compose -f dockercompose.yml up -d
+
+echo -e "####### Application startup completed !!! ######"
+echo -e "\nPHPADMIN: port 8001\n APPLICATION: port: 80\n"
+echo -e "############## Initialization completed!! on: `curl ifconfig.me`.... Enjoy app"
